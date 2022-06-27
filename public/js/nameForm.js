1d0cc7c4843e08a96b895e69e49d24df00bacc31
Menno Vlaming <mennovlaming@gmail.com>
2022-06-27 17:05:18 +0200

    comments

--- public/js/nameForm.js
+++ public/js/nameForm.js
@@ -1,11 +1,7 @@
 let autocomplete;
       function initAutocomplete() {
-        
-
         const inputs = document.querySelectorAll('input')
-
         console.log(inputs);
-    
         inputs.forEach(input => {
           
           new google.maps.places.Autocomplete(input, {
@@ -18,6 +14,7 @@ let autocomplete;
 
       var test = document.getElementById("locatie");
 
+      //Dit is de functie van de button, met geolocatie pakt ie lat en lon
       function getLocation() {
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(showPosition);
@@ -27,16 +24,11 @@ let autocomplete;
         }
         
       }
-
-      
+      //Hier zet ie de huidige lat en lon en de console, dit moet dus uiteindelijk als stad of land in de HTML komen
       function showPosition(position) {
         let currentLatLon = `${position.coords.latitude}, ${position.coords.longitude}`;
         test.innerHTML = 
-        //  "Latitude: " + position.coords.latitude + 
-        //  "<br>Longitude: " + position.coords.longitude;
-   `Latitude: ${position.coords.latitude}<br> Longitude: ${position.coords.longitude}`;
-    
-        
+        `Latitude: ${position.coords.latitude}<br> Longitude: ${position.coords.longitude}`;
         console.log(currentLatLon);
       }
 
@@ -53,7 +45,10 @@ let autocomplete;
   function getData(){
     
     // fetch(url) dus de url van je api
+
+    //ik dacht misschien als ik hier een template string van maak en dan de uitgecommente fetch doe.
     fetch(`http://api.positionstack.com/v1/reverse?access_key=489c94689aedfbbce029bb9ec16310c1&query=52.9618562,4.7517031&country=NLD&output=json`)
+
     // fetch(`http://api.positionstack.com/v1/reverse?access_key=489c94689aedfbbce029bb9ec16310c1&query=${position.coords.latitude},${position.coords.longitude}&country=NLD&output=json`)
      
     // dit is een get request, en dan krijg je een response terug in .then in js gebruik je json dus wil je de response naar json vormen met de method .json() dus response.json()
